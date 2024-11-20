401180aa5f3ecb1beab71a61dbd474ac0c3178d6
webdiller <eugenefromrus@gmail.com>
2024-11-20 14:15:35 +1000

    upd: some breaks and changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/playground-github-actions/", // YOUR REPO NAME HERE
 })
